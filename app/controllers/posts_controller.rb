4ea6c7f86b27c7b72c6d7657c1a053ca06df2637
nolanborz <nolanrborzoni@gmail.com>
2024-12-22 13:08:34 +0900

    Add profile pic and grav api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user).order(created_at: :desc)
+    @show_followed = params[:filter] == "following"
+    @posts = if @show_followed && current_user
+      Post.where(user: [ current_user ] + current_user.following)
+          .includes(:user, :comments)
+          .order(created_at: :desc)
+    else
+      Post.includes(:user, :comments).order(created_at: :desc)
+    end
   end
 
   def show
