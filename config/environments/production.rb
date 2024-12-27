50e4eee284e7f8aba36d4c6d1c8a45f18567233b
nolanborz <nolanrborzoni@gmail.com>
2024-12-27 16:53:12 +0900

    Configure email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,19 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "borzonibook.com",
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
+  config.action_mailer.default_url_options = { host: "borzonibook.com" }
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
