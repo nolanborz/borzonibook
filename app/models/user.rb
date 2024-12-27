875ebfc5b5719ebd84dd5152fccc2b61a209392f
nolanborz <nolanrborzoni@gmail.com>
2024-12-27 20:28:26 +0900

    Temporarily disable welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,6 @@ class User < ApplicationRecord
   end
 
   def send_welcome_email
-    UserMailer.with(user: self).welcome_email.deliver_now
+    # UserMailer.with(user: self).welcome_email.deliver_now
   end
 end
