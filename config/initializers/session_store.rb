2f488042915a631d2eb5e48baef5add9c3ae617f
nolanborz <nolanrborzoni@gmail.com>
2024-12-30 11:27:33 +0900

    Add basic session store configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,2 @@
-Rails.application.config.session_store :cookie_store,
-  key: "_borzonibook_session",
-  expire_after: 24.hours,
-  secure: Rails.env.production?,
-  same_site: :lax
+# config/initializers/session_store.rb
+Rails.application.config.session_store :cookie_store, key: "_borzonibook_session"
