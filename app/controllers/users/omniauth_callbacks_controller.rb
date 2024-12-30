1fb0859cb1d0e85958183337f3180f58d99f02b5
nolanborz <nolanrborzoni@gmail.com>
2024-12-30 12:40:42 +0900

    Adjust double email signup cases

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,17 +3,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
 
-    # Check if user exists
+    # First, try to find an existing user
     @user = User.find_by(
-      provider: request.env["omniauth.auth"].provider,
-      uid: request.env["omniauth.auth"].uid
+      email: request.env["omniauth.auth"].info.email
     )
 
-    if @user&.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
-      sign_in_and_redirect @user, event: :authentication
+    if @user
+      if @user.provider == "google_oauth2"
+        # Existing Google OAuth user - sign them in
+        sign_in_and_redirect @user, event: :authentication
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+      else
+        # User exists but didn't use Google to sign up
+        flash[:alert] = "An account with this email already exists. Please sign in with your password."
+        redirect_to new_user_session_path
+      end
     else
-      # Always redirect to invitation code form for new users
+      # New user - redirect to invitation code form
       redirect_to users_new_oauth_invitation_path
     end
   end
