1c563cc4c7f1a447b11ea83fa8045543728b15a1
nolanborz <nolanrborzoni@gmail.com>
2024-12-30 11:18:59 +0900

    Add session store configuration

--- app/controllers/users/oauth_invitations_controller.rb
+++ app/controllers/users/oauth_invitations_controller.rb
@@ -1,26 +1,36 @@
-class Users::OauthInvitationsController < ApplicationController
-  def new
-    redirect_to root_path, alert: "Please sign in with Google first." if session["devise.google_data"].blank?
+# app/controllers/users/oauth_invitations_controller.rb
+def new
+  if session["devise.google_data"].blank?
+    Rails.logger.error "OAuth data missing from session"
+    redirect_to root_path, alert: "Please sign in with Google first."
+  else
+    Rails.logger.info "OAuth data present in session: #{session["devise.google_data"].keys}"
   end
+end
 
-  def create
-    oauth_data = session["devise.google_data"]
+def create
+  oauth_data = session["devise.google_data"]
+  Rails.logger.info "Creating user with OAuth data: #{oauth_data&.keys}"
+  Rails.logger.info "Received invitation code: #{params[:invitation_code]}"
+  Rails.logger.info "Environment invitation code: #{ENV["SITE_INVITATION_CODE"]}"
 
-    if Rails.application.credentials.invitation_codes.include?(params[:invitation_code])
-      @user = User.from_omniauth(oauth_data)
-      @user.invitation_code = params[:invitation_code]
+  if params[:invitation_code] == ENV["SITE_INVITATION_CODE"]
+    @user = User.from_omniauth(oauth_data)
+    @user.invitation_code = params[:invitation_code]
 
-      if @user.save
-        session["devise.google_data"] = nil
-        sign_in_and_redirect @user, event: :authentication
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
-      else
-        flash.now[:alert] = @user.errors.full_messages.join("\n")
-        render :new
-      end
+    if @user.save
+      Rails.logger.info "User successfully created"
+      session["devise.google_data"] = nil
+      sign_in_and_redirect @user, event: :authentication
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
     else
-      flash.now[:alert] = "Invalid invitation code"
+      Rails.logger.error "User creation failed: #{@user.errors.full_messages}"
+      flash.now[:alert] = @user.errors.full_messages.join("\n")
       render :new
     end
+  else
+    Rails.logger.error "Invalid invitation code provided"
+    flash.now[:alert] = "Invalid invitation code"
+    render :new
   end
 end
